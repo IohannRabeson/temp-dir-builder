71e0094b864db7a026aaffd1d14bbb6bddca62e8
irabeson <iohannrabeson@users.noreply.github.com>
2025-04-12 03:48:46 -0400

    Fix snippets and cleanup doc.

--- src/lib.rs
+++ src/lib.rs
@@ -34,28 +34,10 @@ impl Tree {
 /// let tree_fs = TreeBuilder::default()
 ///     .add_text("test/foo.txt", "bar")
 ///     .add_empty("test/folder-a/folder-b/bar.txt")
+///     .add_file("test/file.rs", file!())
 ///     .create()
 ///     .expect("create tree fs");
 /// println!("created successfully in {}", tree_fs.root().display());
-// </snip>
-/// ```
-/// ```rust
-// <snip id="example-drop">
-/// use tree_fs::TreeBuilder;
-/// let tree_fs = TreeBuilder::default()
-///      .add_text("test/foo.txt", "bar")
-///      .add_empty("test/folder-a/folder-b/bar.txt")
-///      .drop(true)
-///      .create()
-///      .expect("create tree fs");
-///
-/// println!("created successfully in {}", tree_fs.root().display());
-///
-/// let path = tree_fs.root().to_path_buf();
-/// assert!(path.exists());
-///
-/// drop(tree_fs);
-/// assert!(!path.exists());
 // </snip>
 /// ```
 #[derive(Debug)]
