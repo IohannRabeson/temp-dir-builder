86f74067be0e57a9a4396278f199f4ac2a985f8d
irabeson <iohannrabeson@users.noreply.github.com>
2026-09-16 21:10:54 -0400

    add_text_file now passes the tex parameter as impl Into<String>.

--- src/lib.rs
+++ src/lib.rs
@@ -161,9 +161,8 @@ impl TempDirectoryBuilder {
     ///   If the path is outside the created directory (e.g: "../foo") the error `BuildError::EntryOutsideDirectory` will be returned.
     /// * `text` - Text to be written in the new file created.
     #[must_use]
-    #[allow(clippy::needless_pass_by_value)]
-    pub fn add_text_file(self, path: impl AsRef<Path>, text: impl ToString) -> EntryBuilder {
-        self.add(path, Kind::TextFile(text.to_string()))
+    pub fn add_text_file(self, path: impl AsRef<Path>, text: impl Into<String>) -> EntryBuilder {
+        self.add(path, Kind::TextFile(text.into()))
     }
 
     /// Adds a binary file specifying the content.
@@ -327,7 +326,7 @@ impl EntryBuilder {
 
     /// Adds a text file specifying the content.
     #[must_use]
-    pub fn add_text_file(self, path: impl AsRef<Path>, text: impl ToString) -> Self {
+    pub fn add_text_file(self, path: impl AsRef<Path>, text: impl Into<String>) -> Self {
         self.builder.add_text_file(path, text)
     }
 
